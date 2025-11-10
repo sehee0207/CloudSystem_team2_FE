d5875403da317f1cd3c6b11ddac8ab8576184319
sehee0207 <lolry0207@gmail.com>
2025-11-10 23:07:42 +0900

    feautre 디자인 변경

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,40 +1,50 @@
 const features = [
   {
-    title: "로그인 없이 간단한 사용",
-    description: "계정 생성의 번거로움 없이 바로 서비스를 이용해 보세요! 꾸준한 학습이 가능합니다.",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+      </svg>
+    ),
+    title: "로그인 없는 간단한 사용",
+    description: "계정 생성의 번거로움 없이 이메일 구독 신청을 통해 바로 서비스를 이용해 보세요!",
   },
   {
-    title: "AI 기반 분야에 맞는 질문 생성",
-    description: "매일 오전 8시, AI/클라우드/CS 중 1개의 질문을 받아보세요! 매일매일 달라지는 분야별 질문을 체험하세요.",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width={1.5} stroke="currentColor" className="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+      </svg>
+    ),
+    title: "AI 기반, 선택 분야에 맞는 질문 생성",
+    description: "평일 오전 8시, AI/클라우드/CS 중 1개의 질문을 받아보세요!",
   },
   {
-    title: "AI 기반 답변 확인 및 피드백 제공",
-    description: "제한 시간 내에 오늘의 질문에 대한 답변을 입력하세요! 인공지능이 답변을 분석하여 = 구체적인 피드백을 제공합니다.",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width={1.5} stroke="currentColor" className="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12" />
+      </svg>
+    ),
+    title: "답변 확인 및 피드백 제공",
+    description: "메일의 링크를 통해 접근하여 제한 시간 내에 오늘의 질문에 대한 답변을 입력하세요! 인공지능이 답변을 분석하여 구체적인 피드백을 제공합니다.",
   },
 ];
 
 export function Features() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32">
+    <section className="w-full min-h-[calc(100vh-5rem)] py-12 md:py-24 lg:py-32">
       <div className="container px-4 md:px-6 mx-auto">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
-          <div className="space-y-10">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-gray-800">
-              Bit-Bite 주요 기능
-            </h2>
-            <p className="max-w-[900px] text-gray-600 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              AI와 함께 CS 지식을 설명하는 습관을 만들어보세요.
-            </p>
-            <p className="max-w-[900px] text-gray-600 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              Bit-Bite는 다음과 같은 핵심 기능을 제공합니다.
-            </p>
-          </div>
+          <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl text-gray-800">
+            Bit-Bite 주요 기능
+          </h2>
         </div>
-        <div className="mx-auto grid max-w-7xl items-start gap-8 py-12 lg:grid-cols-3">
+        <div className="mx-auto grid max-w-7xl items-stretch gap-8 py-12 lg:grid-cols-3">
           {features.map((feature) => (
             <div key={feature.title} className="grid gap-4 p-6 rounded-lg shadow-lg bg-white">
-              <h3 className="text-lg font-bold text-gray-900">{feature.title}</h3>
-              <p className="text-sm text-gray-600">{feature.description}</p>
+              <div className="flex items-center justify-center h-12 w-12 rounded-full bg-main text-white">
+                <span className="h-6 w-6">{feature.icon}</span>
+              </div>
+              <h3 className="text-lg font-bold text-gray-800">{feature.title}</h3>
+              <p className="text-sm text-gray-500">{feature.description}</p>
             </div>
           ))}
         </div>
